b203bac06a015285915541aca91bed1b3f5e2bfc
David <davidvo1992@gmail.com>
2023-04-23 20:21:04 -0500

    to submit lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  const leftNumbers2 = dodger.style.left.replace("px", "");
+  const left2 = parseInt(leftNumbers2, 10);
+
+  if (left2 > 0) {
+    dodger.style.left = `${left2 + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e2) {
+  if (e2.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
